2ee3602ef656cfcc3aeadb585961cbbbda0a3efe
MarcelRx <167208351+MarcelRx@users.noreply.github.com>
2025-04-16 14:36:58 +0200

    Update Credentials.cs

--- formbridge/server/Credentials.cs
+++ formbridge/server/Credentials.cs
@@ -1,1 +1,10 @@
+namespace server;
 
+        public static class Credentials
+        {
+            public static readonly string Host = "";
+            public static readonly string Port = "";
+            public static readonly string Username = "";
+            public static readonly string Password = "";
+            public static readonly string Database = "";
+        }
